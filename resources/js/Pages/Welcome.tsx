30aa8693302ff91e71fbcbec1d4b6e8f1ab06911
Arka Barua <arka.barua@northsouth.edu>
2023-08-12 17:44:36 +0600

    Auth setup and theme change setup added

---
+++ resources/js/Pages/Welcome.tsx
@@ -0,0 +1,76 @@
+import { Link, Head } from "@inertiajs/react";
+import { PageProps } from "@/types";
+import { Button } from "@/Components/ui/button";
+import { Moon, Sun } from "lucide-react";
+
+import {
+    DropdownMenu,
+    DropdownMenuContent,
+    DropdownMenuItem,
+    DropdownMenuTrigger,
+} from "@/Components/ui/dropdown-menu";
+import { useTheme } from "@/Components/theme-provider";
+
+export default function Welcome({
+    auth,
+}: PageProps<{ laravelVersion: string; phpVersion: string }>) {
+    const { setTheme } = useTheme();
+    return (
+        <>
+            <Head title="Welcome" />
+            <div className="relative sm:flex sm:justify-center sm:items-center min-h-screen bg-dots-darker bg-center bg-gray-100 dark:bg-dots-lighter dark:bg-gray-900 selection:bg-red-500 selection:text-white">
+                <div className="flex justify-start items-center gap-4 top-0 fixed left-0 py-6 pl-12">
+                    <p className="font-medium text-xl">Hotel-BA</p>
+                    <DropdownMenu>
+                        <DropdownMenuTrigger asChild>
+                            <Button variant="outline" size="icon">
+                                <Sun className="h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0" />
+                                <Moon className="absolute h-[1.2rem] w-[1.2rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100" />
+                                <span className="sr-only">Toggle theme</span>
+                            </Button>
+                        </DropdownMenuTrigger>
+                        <DropdownMenuContent align="end">
+                            <DropdownMenuItem onClick={() => setTheme("light")}>
+                                Light
+                            </DropdownMenuItem>
+                            <DropdownMenuItem onClick={() => setTheme("dark")}>
+                                Dark
+                            </DropdownMenuItem>
+                            <DropdownMenuItem
+                                onClick={() => setTheme("system")}
+                            >
+                                System
+                            </DropdownMenuItem>
+                        </DropdownMenuContent>
+                    </DropdownMenu>
+                </div>
+                <div className="sm:fixed sm:top-0 sm:right-0 p-6 text-right">
+                    {auth.user ? (
+                        <Link
+                            href={route("dashboard")}
+                            className="font-semibold text-gray-600 hover:text-gray-900 dark:text-gray-400 dark:hover:text-white focus:outline focus:outline-2 focus:rounded-sm focus:outline-red-500"
+                        >
+                            Dashboard
+                        </Link>
+                    ) : (
+                        <>
+                            <Link
+                                href={route("login")}
+                                className="font-semibold text-gray-600 hover:text-gray-900 dark:text-gray-400 dark:hover:text-white focus:outline focus:outline-2 focus:rounded-sm focus:outline-red-500"
+                            >
+                                Log in
+                            </Link>
+
+                            <Link
+                                href={route("register")}
+                                className="ml-4 font-semibold text-gray-600 hover:text-gray-900 dark:text-gray-400 dark:hover:text-white focus:outline focus:outline-2 focus:rounded-sm focus:outline-red-500"
+                            >
+                                Register
+                            </Link>
+                        </>
+                    )}
+                </div>
+            </div>
+        </>
+    );
+}
